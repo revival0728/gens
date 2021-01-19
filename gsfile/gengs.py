190b10e612009495184ccf5619c9766114629dbb
revival0728 <revival0728@gmail.com>
2021-01-19 18:46:11 +0800

    add gengs.py and defaultcode.txt (including user guide)

---
+++ gsfile/gengs.py
@@ -0,0 +1,44 @@
+#this file is for generating source file
+
+import os
+import sys
+import getopt
+
+fn = ""
+addr = ""
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:a:", ["filename=", "address="])
+    for k, v in opts:
+        if k == "-n":
+            fn = v
+        elif k == "-a":
+            addr = v
+
+except getopt.GetoptError:
+    print("Something Went Wrong")
+    quit()
+
+if fn == "" or addr == "":
+    print("Wrong Command")
+    quit()
+
+source = open(addr+"\\"+fn, "w")
+dfc = open("defaultcode.txt", "r")
+
+_fn = ""
+
+if fn.find(".") == -1:
+    print("Missing file extension")
+    quit()
+else:
+    _fn = fn[:fn.find(".")]
+
+for i in dfc.readlines():
+    s = i
+    s = s.replace("\r", "")
+    if not s.find("*FILENAME*") == -1:
+        s = s.replace("*FILENAME*", _fn)
+    source.write(s)
+
+source.close()
---
+++ gsfile/test.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+#define fileio
+#ifdef fileio
+ifstream fin(test_in.txt);
+ofstream fout(test_out.txt);
+#define cin fin
+#define cout fout
+#endif
+
+void solve() {
+	;
+}
+
+int main() {
+#ifdef fileio
+	cin.tie(0), ios_base::sync_with_stdio(0);
+#endif
+	solve();
+}
+
